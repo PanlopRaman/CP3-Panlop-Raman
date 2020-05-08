57225b8239587d6a8df1e26f7f3a5d7d9cc1145d
PanlopRaman <64823368+PanlopRaman@users.noreply.github.com>
2020-05-08 16:12:10 +0700

    Create Exercise5_2_Panlop_R.py

---
+++ Exercise5_2_Panlop_R.py
@@ -0,0 +1,5 @@
+D = float(input("input distance (km.) : "))
+T = float(input("input time (hr.) : "))
+print("-------------------------------")
+print("Your speed is : ", D/T, "km/hr")
+print("-------------------------------")
