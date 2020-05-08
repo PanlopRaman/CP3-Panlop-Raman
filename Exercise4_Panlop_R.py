17b3b75b73c24f3cc014f8869257b9fe04d60dd7
PanlopRaman <64823368+PanlopRaman@users.noreply.github.com>
2020-05-08 13:06:35 +0700

    Create Exercise4_Panlop_R.py

---
+++ Exercise4_Panlop_R.py
@@ -0,0 +1,10 @@
+FE = 88.0
+GB = 93.5
+ICS = 91.5
+CP = 98.0
+print("---- Subject ----", "            Your Score")
+print("Foundation English               :", FE)
+print("General Business                 :", GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming             :", CP)
+print("Your AVG. Score is : ", (FE+GB+ICS+CP)/4)
