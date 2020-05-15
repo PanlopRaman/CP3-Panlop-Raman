a68de8ca5eff7b60702b58b2d88c1aa7137adec0
PanlopRaman <64823368+PanlopRaman@users.noreply.github.com>
2020-05-15 11:31:03 +0700

    Update Lecture53_Panlop_R.py

--- Lecture53_Panlop_R.py
+++ Lecture53_Panlop_R.py
@@ -2,4 +2,4 @@ price = float(input("Enter product price: "))
 def vatCal(TP):
     result = TP+(TP*7/100)
     return result
-print("Total Price includea VAT : ", (vatCal(price)), "THB")
+print("Total Price included VAT : ", (vatCal(price)), "THB")
